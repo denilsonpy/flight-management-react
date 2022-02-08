9caf22266a6744dbf03cc8713e46f33c80dc4128
denilsonpy <contato.denilsonsilva@gmail.com>
2022-02-08 20:12:08 -0300

    feat(themes): add theme config

---
+++ src/constants/themes.ts
@@ -0,0 +1,14 @@
+export const themes = {
+  colors: {
+    primary: "#E7ECEF",
+    secondary: "#274C77",
+
+    halfSecondary: "#516F91",
+    secondaryLow: "#879CB3",
+
+    light: "#F1F4F5",
+
+    grayDark: "#666E71",
+    grayLight: "#A8B1B3",
+  },
+};
