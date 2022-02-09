d235ce4204905e31df79a2f8ccead73b73177968
denilsonpy <contato.denilsonsilva@gmail.com>
2022-02-08 21:25:32 -0300

    feat(initial): project config

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,14 +1,16 @@
-export const themes = {
+const themes = {
   colors: {
-    primary: "#E7ECEF",
-    secondary: "#274C77",
+    primary: '#E7ECEF',
+    secondary: '#274C77',
 
-    halfSecondary: "#516F91",
-    secondaryLow: "#879CB3",
+    halfSecondary: '#516F91',
+    secondaryLow: '#879CB3',
 
-    light: "#F1F4F5",
+    light: '#F8F8FB',
 
-    grayDark: "#666E71",
-    grayLight: "#A8B1B3",
+    grayDark: '#666E71',
+    grayLight: '#A8B1B3',
   },
 };
+
+export default themes;
