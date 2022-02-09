4c403037519162a30379fc166e6296c6228366cf
Denilson Martins <contato.denilsonsilva@gmail.com>
2022-02-08 21:27:25 -0300

    Project config (#2)
    * feat(themes): add theme config
    
    * feat(initial): project config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Container className="App">
+      <Dashboard />
+    </Container>
+  );
+}
+
+export default App;
