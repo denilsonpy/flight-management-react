ffacd0886465838587d1d9368ac045425d56cbf3
denilsonpy <contato.denilsonsilva@gmail.com>
2022-02-15 13:55:27 -0300

    feat(): add weather and wind card design

--- src/components/DashboardGrid/index.tsx
+++ src/components/DashboardGrid/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Grid, GridItem, Box } from '@chakra-ui/react';
+import {
+  Grid, GridItem, Box, Text, LinkBox, LinkOverlay,
+} from '@chakra-ui/react';
+import { IoIosArrowForward } from 'react-icons/io';
+import { WiDaySunny } from 'react-icons/wi';
+import { GiWindsock } from 'react-icons/gi';
+
 import themes from '../../constants/themes';
 
 // import themes from '../../constants/themes';
@@ -26,14 +32,135 @@ export default function DashboardGrid() {
       templateColumns="repeat(9, 1fr)"
       gap={4}
     >
-      <GridItem colSpan={3} rowSpan={2} display="flex" alignItems="center" justifyContent="center">
-        <Box width={350} height={200} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
+      <GridItem
+        colSpan={3}
+        rowSpan={2}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        marginTop={4}
+      >
+        <LinkBox width={250} height={150} padding={3} boxShadow="xl" bg={themes.colors.light} borderRadius={15}>
+          <Box flexDirection="row" display="flex" justifyContent="space-between" alignItems="center">
+            <Text
+              as="h1"
+              fontWeight="bold"
+              color={themes.colors.secondary}
+              fontSize={20}
+            >
+              Weather
+            </Text>
+            <IoIosArrowForward color={themes.colors.grayDark} />
+          </Box>
+          <Box flexDirection="row" display="flex" justifyContent="space-between" alignItems="center">
+            <Box flexDirection="column" display="flex" justifyContent="space-between" alignItems="start">
+              <Text
+                as="p"
+                color={themes.colors.grayDark}
+                fontSize={18}
+              >
+                Rasht
+              </Text>
+              <Text
+                as="p"
+                color={themes.colors.secondary}
+                fontWeight="bold"
+                fontSize={24}
+              >
+                14° C
+              </Text>
+              <Box flexDirection="row" display="flex" gap={3} color={themes.colors.grayLight}>
+                <Text
+                  as="p"
+                  color="blue.300"
+                  fontWeight="bold"
+                  fontSize={18}
+                >
+                  13° C
+                </Text>
+                /
+                <Text
+                  as="p"
+                  color="orange"
+                  fontWeight="bold"
+                  fontSize={18}
+                >
+                  14° C
+                </Text>
+              </Box>
+
+            </Box>
+            <LinkOverlay href="https://www.redemet.aer.mil.br/" target="_blank" title="metar" flexDirection="column" display="flex" justifyContent="space-between" alignItems="start">
+              <WiDaySunny fontSize={80} color="orange" />
+            </LinkOverlay>
+          </Box>
+        </LinkBox>
       </GridItem>
-      <GridItem colSpan={3} rowSpan={2} display="flex" alignItems="center" justifyContent="center">
-        <Box width={350} height={200} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
+      <GridItem
+        colSpan={3}
+        rowSpan={2}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        marginTop={4}
+      >
+        <LinkBox
+          width={250}
+          height={150}
+          padding={3}
+          boxShadow="xl"
+          bg={themes.colors.light}
+          borderRadius={15}
+        >
+          <Box flexDirection="row" display="flex" justifyContent="space-between" alignItems="center">
+            <Text
+              as="h1"
+              fontWeight="bold"
+              color={themes.colors.secondary}
+              fontSize={20}
+            >
+              Wind
+            </Text>
+            <IoIosArrowForward color={themes.colors.grayDark} />
+          </Box>
+          <Box flexDirection="row" display="flex" justifyContent="space-between" alignItems="center">
+
+            <Box flexDirection="column" display="flex" justifyContent="space-between" alignItems="start">
+              <Text
+                as="p"
+                color={themes.colors.grayDark}
+                fontSize={18}
+              >
+                Speed
+              </Text>
+              <Text
+                as="p"
+                color={themes.colors.secondary}
+                fontWeight="bold"
+                fontSize={24}
+              >
+                1.5 km
+              </Text>
+              <Box flexDirection="row" display="flex" gap={3} color={themes.colors.grayLight}>
+                <Text
+                  as="p"
+                  color="blue.300"
+                  fontWeight="bold"
+                  fontSize={18}
+                >
+                  350 deg
+                </Text>
+              </Box>
+
+            </Box>
+            <LinkOverlay href="https://www.windy.com/SDRK" target="_blank" title="windy" flexDirection="column" display="flex" justifyContent="space-between" alignItems="start">
+              <GiWindsock fontSize={80} color={themes.colors.secondary} />
+            </LinkOverlay>
+          </Box>
+        </LinkBox>
       </GridItem>
       <GridItem colSpan={3} rowSpan={3} display="flex" alignItems="flex-start" justifyContent="center">
-        <Box width={250} height="90%" boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width={250} height="90%" boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
       </GridItem>
       <GridItem
         colSpan={6}
@@ -43,15 +170,14 @@ export default function DashboardGrid() {
         flexDirection="column"
         justifyContent="center"
         gap={8}
-        marginTop={30}
       >
-        <Box width="85%" height={100} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
-        <Box width="85%" height={100} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
-        <Box width="85%" height={100} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
-        <Box width="85%" height={100} boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width="85%" height={75} boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width="85%" height={75} boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width="85%" height={75} boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width="85%" height={75} boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
       </GridItem>
       <GridItem colSpan={3} rowSpan={5} display="flex" alignItems="flex-start" justifyContent="center">
-        <Box width={250} height="90%" boxShadow="2xl" bg={themes.colors.light} borderRadius={15} />
+        <Box width={250} height="90%" boxShadow="xl" bg={themes.colors.light} borderRadius={15} />
       </GridItem>
     </Grid>
   );
